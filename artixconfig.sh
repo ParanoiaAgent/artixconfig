48e399585294dfe22f9e7070ecb671babe0289a7
Paranoia Agent <86332994+ParanoiaAgent@users.noreply.github.com>
2021-06-29 16:30:00 -0300

    Create artixconfig.sh

---
+++ artixconfig.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+### Updating System
+sudo pacman -Syu --noconfirm
+
+echo "Finished System Update"
+
+### YAY Install
+cd
+mkdir -p Downloads/Git
+cd Downloads/Git
+
+git clone https://aur.archlinux.org/yay.git
+sudo chown -R paranoia-agent:users ./yay
+cd yay
+makepkg -si --noconfirm
+
+echo "Finished YAY Install"
+
+### Pacman Packages
+sudo pacman -S  bspwm sxhkd rofi dunst sddm-runit xfce4-power-manager xfce4-settings firefox thunar thunar-archive-plugin gvfs-mtp ranger w3m ffmpegthumbnailer feh neofetch pulseaudio pulseaudio-alsa pavucontrol polkit-gnome mousepad vim htop wget iwd-runit wireless_tools wpa_supplicant-runit xdg-utils xdg-user-dirs xf86-input-wacom noto-fonts noto-fonts-cjk
+
+echo "Finished Installing Pacman Packages"
+
+### AUR Packages
+yay -S  picom-jonaburg-git polybar playerctl-git zscroll-git kitty-git udevil-git flameshot-git starship-git pywal-git zathura-git zathura-pdf-poppler-git rofi-dmenu networkmanager-dmenu-git i3lock-color xidlehook matrix-mirage themix-full-git bibata-cursor-theme nerd-fonts-hack nerd-fonts-jetbrains-mono
+
+echo "Finished Installing AUR Packages"
+
+sudo pacman -Rns i3 --noconfirm
+
+cd
+sudo chmod +x .config/polybar/player.sh
+sudo chmod +x .config/i3lock/lockscreen
+sudo chmod +x .config/bspwm/bspwmrc
+
+sudo systemctl enable sddm
+ln -s /etc/runit/sv/sddm/ /run/runit/service
+
+echo "Finished Setup"
